483af48e0e1d22de49417e2eee422732c7571281
anugerahbasy <121616431+anugerahbasy@users.noreply.github.com>
2026-04-16 14:01:32 +0700

    commit 2

---
+++ index.js
@@ -0,0 +1,8 @@
+const mealContainer = document.getElementById("");
+const loading = document.getElementById("loading");
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn");
+
+// Event
+searchBtn.addEventListener("click", searchMeal);
+
