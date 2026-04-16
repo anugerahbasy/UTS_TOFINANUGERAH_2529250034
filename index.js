4c73dacbf026ded35ae5401e14614712efa883e6
anugerahbasy <121616431+anugerahbasy@users.noreply.github.com>
2026-04-16 14:40:59 +0700

    commit 3

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const mealContainer = document.getElementById("");
+const mealContainer = document.getElementById("mealContainer");
 const loading = document.getElementById("loading");
 const searchInput = document.getElementById("searchInput");
 const searchBtn = document.getElementById("searchBtn");
@@ -6,3 +6,29 @@ const searchBtn = document.getElementById("searchBtn");
 // Event
 searchBtn.addEventListener("click", searchMeal);
 
+searchInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") searchMeal();
+});
+
+// Load awal
+window.addEventListener("DOMContentLoaded", loadDefaultMeals);
+
+async function loadDefaultMeals() {
+  try {
+    const res = await fetch("https://gist.githubusercontent.com/nurrachmat/f954f3aa979ab2a05daacae722418e72/raw/2cf704f88aa1a34255c281772abea2b88e121b76/harga-emas-maret2026.json"
+    );
+    const data = await res.json();
+    if (data.meals) displayMeals(data.meals);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function searchMeal() {
+  const query = searchInput.value.trim();
+
+  if (!query) {
+    alert("Masukkan nama makanan!");
+    return;
+  }
+}
